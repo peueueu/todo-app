a328eab9dad1f3c0874f031d44e23816d22281e2
Pedro Silva <psilva@thoughtworks.com>
2024-04-08 11:00:19 -0300

    feat: :sparkles: add initial authentication flow for the todoapp

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,11 +1,12 @@
-import base64
+from typing import Annotated
 
-from fastapi import APIRouter, HTTPException, status
+from bcrypt import checkpw, gensalt, hashpw
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel, Field
 
 from ..db.database import db_dependency
 from ..db.models import Users
-from bcrypt import hashpw, gensalt
 
 router = APIRouter(prefix="/auth")
 
@@ -19,6 +20,16 @@ class CreateUserRequest(BaseModel):
     role: str
 
 
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    hashed_password = password.encode("utf-8")
+    if not checkpw(hashed_password, user.password.encode("utf-8")):
+        return False
+    return True
+
+
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(**create_user_request.model_dump())
@@ -28,7 +39,9 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Not possible to create user.",
         )
-    hashed_password = hashpw(password=create_user_request.password.encode("utf-8"), salt=gensalt()).decode("utf-8")
+    hashed_password = hashpw(
+        password=create_user_request.password.encode("utf-8"), salt=gensalt()
+    ).decode("utf-8")
     create_user_model.password = hashed_password
 
     db.add(create_user_model)
@@ -38,5 +51,12 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
 
 
 @router.post("/signin", status_code=status.HTTP_200_OK)
-async def signin(db: db_dependency, signin_user_request):
-    ...
+async def signin(
+    db: db_dependency, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(
+        username=form_data.username, password=form_data.password, db=db
+    )
+    if not user:
+        return "Failed Authentication"
+    return "Successful Authentication"
